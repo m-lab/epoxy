5c2d2adb248e4a198e5a7fc3f677d1edda83d9db
Stephen Soltesz <soltesz@google.com>
2016-12-27 18:37:07 -0500

    absdir.

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,12 @@
 set -x
 set -e
 
+env
+pwd
+
 gcloud auth activate-service-account --key-file /tmp/mlab-sandbox-appengine-deploy.json
 
-pushd cmd/epoxy_boot_server
+pushd $TRAVIS_BUILD_DIR/cmd/epoxy_boot_server
   gcloud --verbosity debug --project mlab-sandbox --quiet app deploy --promote app.yaml
 popd
 

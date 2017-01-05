0859cdd7436117feece531b2006d11b1e1849102
Stephen Soltesz <soltesz@google.com>
2017-01-04 19:41:23 -0500

    Clarify comments, add helpful notes and references.

--- travis/deploy_app.sh
+++ travis/deploy_app.sh
@@ -13,9 +13,12 @@ BASEDIR=${3:?Please provide the base directory containing app.yaml}
 source "${HOME}/google-cloud-sdk/path.bash.inc"
 
 # All operations are performed as the service account named in KEYFILE.
+# For all options see:
+# https://cloud.google.com/sdk/gcloud/reference/auth/activate-service-account
 gcloud auth activate-service-account --key-file "${KEYFILE}"
 
-# For all options see: https://cloud.google.com/sdk/gcloud/reference/config/set
+# For all options see:
+# https://cloud.google.com/sdk/gcloud/reference/config/set
 gcloud config set core/project "${PROJECT}"
 gcloud config set core/disable_prompts true
 gcloud config set core/verbosity debug
@@ -23,6 +26,8 @@ gcloud config set core/verbosity debug
 # Make build artifacts available to docker build.
 pushd "${BASEDIR}"
   # Automatically promote the new version to "serving".
+  # For all options see:
+  # https://cloud.google.com/sdk/gcloud/reference/app/deploy
   gcloud app deploy --promote app.yaml
 popd
 

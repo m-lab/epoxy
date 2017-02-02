e005599976da21027713926b2f7495afd5dbe794
Stephen Soltesz <soltesz@google.com>
2017-02-01 19:59:16 -0500

    Update epoxy_boot_server to use Datastore.
    This change adds support for reading Host records from Datastore and
    moves the GenerateStage2IPXE handler to a separate package.
    
    As-is, this change should allow ePoxy clients to boot to a stage2 image.

--- cmd/epoxy_boot_server/main_test.go
+++ cmd/epoxy_boot_server/main_test.go
@@ -15,11 +15,8 @@
 package main
 
 import (
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
-	"strings"
 	"testing"
 )
 
@@ -34,32 +31,5 @@ func TestCheckHealth(t *testing.T) {
 	}
 	if w.Body.String() != "ok" {
 		t.Errorf("wrong health response: got %s want 'ok'", w.Body.String())
-	}
-}
-
-func TestGenerateStage2IPXE(t *testing.T) {
-	r := newRouter()
-	ts := httptest.NewServer(r)
-	defer ts.Close()
-
-	// TODO(soltesz): simulate the values POSTed by an iPXE client.
-	vals := url.Values{}
-	u := ts.URL + "/v1/boot/example.com/stage2.ipxe"
-
-	resp, err := http.PostForm(u, vals)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		t.Fatalf("wrong status code: got %d want %d", resp.StatusCode, http.StatusOK)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !strings.HasPrefix(string(body), "#!ipxe") {
-		t.Errorf("wrong script prefix: got '%s' want '#!ipxe'", body[:6])
 	}
 }

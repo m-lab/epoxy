96ea289bbb9eb4c08f55ac301db55ad1da2cc7db
Stephen Soltesz <soltesz@google.com>
2018-06-11 18:09:31 -0400

    Add last boot and last success metrics from datastore

---
+++ metrics/metrics.go
@@ -0,0 +1,106 @@
+// Copyright 2016 ePoxy Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//////////////////////////////////////////////////////////////////////////////
+
+// Package metrics contains prometheus metric definitions for the epoxy server.
+package metrics
+
+import (
+	"log"
+
+	"github.com/m-lab/epoxy/storage"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	// Stage1Total counts the number of host boots.
+	Stage1Total = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "epoxy_stage1_total",
+			Help: "Total number of boots per machine.",
+		},
+		// Machine name.
+		[]string{"machine"},
+	)
+
+	// RequestDuration profiles request latency.
+	RequestDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: "epoxy_request_duration_seconds",
+			Help: "A histogram of request latencies.",
+			// Note: use default buckets.
+		},
+		[]string{"code"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(Stage1Total)
+	prometheus.MustRegister(RequestDuration)
+}
+
+// Config provides access to Host records.
+type Config interface {
+	List() ([]*storage.Host, error)
+}
+
+// Collector defines a custom collector for reading metrics from datastore.
+type Collector struct {
+	name   string
+	desc   *prometheus.Desc
+	config Config
+}
+
+// NewCollector creates a new datastore collector instance. The metricName should
+// be one of "epoxy_last_boot" or "epoxy_last_success".
+func NewCollector(metricName string, config Config) *Collector {
+	return &Collector{
+		name:   metricName,
+		desc:   nil,
+		config: config,
+	}
+}
+
+// Describe satisfies the prometheus.Collector interface. Describe is called
+// immediately after registering the collector.
+func (col *Collector) Describe(ch chan<- *prometheus.Desc) {
+	if col.desc == nil {
+		col.desc = prometheus.NewDesc(col.name, "The last timestamp for "+col.name, []string{"machine"}, nil)
+	}
+	ch <- col.desc
+}
+
+// Collect satisfies the prometheus.Collector interface. Collect reports values
+// from hosts datastore.
+func (col *Collector) Collect(ch chan<- prometheus.Metric) {
+	hosts, err := col.config.List()
+	if err != nil {
+		log.Println("Failed to list hosts", err)
+		return
+	}
+	for i := range hosts {
+		var ts float64
+		switch col.name {
+		case "epoxy_last_boot":
+			ts = float64(hosts[i].LastSessionCreation.UnixNano()) / 1e9
+		case "epoxy_last_success":
+			ts = float64(hosts[i].LastSuccess.UnixNano()) / 1e9
+		default:
+			log.Println("Unknown collector name:", col.name)
+			return
+		}
+		ch <- prometheus.MustNewConstMetric(
+			col.desc, prometheus.GaugeValue, ts, hosts[i].Name)
+	}
+}
---
+++ metrics/metrics_test.go
@@ -0,0 +1,94 @@
+// Copyright 2016 ePoxy Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//////////////////////////////////////////////////////////////////////////////
+
+package metrics
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/m-lab/epoxy/storage"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// fakeConfig emulates the storage.Config interface for unit tests.
+type fakeConfig struct {
+	host *storage.Host
+}
+
+// List returns a copy of the fakeConfig host.
+func (f fakeConfig) List() ([]*storage.Host, error) {
+	h := make([]*storage.Host, 1)
+	h[0] = f.host
+	return h, nil
+}
+
+func TestNewCollector(t *testing.T) {
+	tests := []struct {
+		name       string
+		metricName string
+		want       string
+	}{
+		{
+			name:       "success",
+			metricName: "epoxy_last_boot",
+			want:       `epoxy_last_boot{machine="mlab1.foo01"}`,
+		},
+		{
+			name:       "success",
+			metricName: "epoxy_last_success",
+			want:       `epoxy_last_success{machine="mlab1.foo01"}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cfg := fakeConfig{
+				host: &storage.Host{
+					Name:                "mlab1.foo01",
+					LastSessionCreation: time.Now(),
+					LastSuccess:         time.Now(),
+				},
+			}
+			prometheus.MustRegister(NewCollector(tt.metricName, cfg))
+			ts := httptest.NewServer(promhttp.Handler())
+			resp, err := http.Get(ts.URL)
+			if err != nil {
+				t.Fatalf("Metrics request failed: %v", err)
+			}
+			b, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("Failed to read results: %v", err)
+			}
+			msg := string(b)
+			lines := strings.Split(msg, "\n")
+			success := false
+			for i := range lines {
+				if strings.HasPrefix(lines[i], tt.want) {
+					success = true
+					break
+				}
+			}
+			if !success {
+				t.Fatalf("Failed to find: %v", tt.want)
+			}
+		})
+	}
+}

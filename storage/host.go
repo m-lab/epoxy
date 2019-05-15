6cf670f49e8ccd64d896a10a1a720ec0348b660c
Stephen Soltesz <soltesz@google.com>
2019-05-15 19:07:06 -0400

    Change CollectedInformation to a map with PropertyLoadSaver interface

--- storage/host.go
+++ storage/host.go
@@ -22,7 +22,13 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
+	"log"
+	"net/url"
+	"strings"
 	"time"
+	"unicode/utf8"
+
+	"cloud.google.com/go/datastore"
 )
 
 // These variables provide indirection for the default function implementations.
@@ -34,19 +40,43 @@ var (
 
 // CollectedInformation stores information received directly from iPXE clients.
 // Field names correspond to iPXE variable names.
-type CollectedInformation struct {
-	Platform         string
-	BuildArch        string
-	Serial           string
-	Asset            string
-	UUID             string
-	Manufacturer     string
-	Product          string
-	Chip             string
-	MAC              string
-	IP               string
-	Version          string
-	PublicSSHHostKey string
+type CollectedInformation map[string]string
+
+var collectedInformationWhitelist = map[string]bool{
+	"platform":            true,
+	"buildarch":           true,
+	"serial":              true,
+	"asset":               true,
+	"uuid":                true,
+	"manufacturer":        true,
+	"product":             true,
+	"chip":                true,
+	"mac":                 true,
+	"ip":                  true,
+	"version":             true,
+	"public_ssh_host_key": true,
+}
+
+// Load sets the given properties in the CollectedInformation instance.
+func (x *CollectedInformation) Load(ps []datastore.Property) error {
+	*x = make(map[string]string)
+	m := *x
+	for _, p := range ps {
+		m[p.Name] = p.Value.(string)
+	}
+	return nil
+}
+
+// Save converts the CollectedInformation datastore properties.
+func (x *CollectedInformation) Save() ([]datastore.Property, error) {
+	var d []datastore.Property
+	for k, v := range *x {
+		d = append(d, datastore.Property{
+			Name:  k,
+			Value: v,
+		})
+	}
+	return d, nil
 }
 
 // TODO: SessionIDs and Sequence structs should be map[string]string, that
@@ -147,6 +177,21 @@ func (h *Host) CurrentSequence() Sequence {
 		return h.Update
 	}
 	return h.Boot
+}
+
+// AddInformation adds values to the Host's CollectedInformation. Only key
+// names in CollectedInformationWhitelist will be added.
+func (h *Host) AddInformation(values url.Values) {
+	for key, values := range values {
+		value := strings.TrimSpace(strings.Join(values, " "))
+		if !utf8.ValidString(value) {
+			log.Printf("Skipping invalid value for: %s CollectedInformation.%s\n", h.Name, key)
+			continue
+		}
+		if collectedInformationWhitelist[key] && value != "" {
+			h.CollectedInformation[key] = value
+		}
+	}
 }
 
 // randomSessionByteCount is the number of bytes used to generate random session IDs.

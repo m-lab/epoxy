bce0134f2b590b934a17cabf0c07583af6b114e7
Stephen Soltesz <stephen.soltesz@gmail.com>
2019-05-15 22:02:53 -0400

    Add unit tests for datastorex

---
+++ datastorex/propertymap_test.go
@@ -0,0 +1,56 @@
+// Package datastorex extends the cloud.google.com/go/datastore package.
+package datastorex
+
+import (
+	"reflect"
+	"testing"
+
+	"cloud.google.com/go/datastore"
+)
+
+func TestMap_Load(t *testing.T) {
+	ps := []datastore.Property{
+		{
+			Name:  "a",
+			Value: "b",
+		},
+		{
+			Name:  "c",
+			Value: "d",
+		},
+	}
+
+	m := Map{}
+	if err := m.Load(ps); err != nil {
+		t.Errorf("Map.Load() error = %v, want nil", err)
+	}
+
+	expected := Map{"a": "b", "c": "d"}
+	if !reflect.DeepEqual(m, expected) {
+		t.Errorf("Map.Load() got = %#v, want %#v", m, expected)
+	}
+}
+
+func TestMap_Save(t *testing.T) {
+	m := Map{"a": "b", "c": "d"}
+
+	got, err := m.Save()
+	if err != nil {
+		t.Errorf("Map.Save() error = %v, want nil", err)
+		return
+	}
+
+	expected := []datastore.Property{
+		{
+			Name:  "a",
+			Value: "b",
+		},
+		{
+			Name:  "c",
+			Value: "d",
+		},
+	}
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("Map.Save() = %#v, want %#v", got, expected)
+	}
+}

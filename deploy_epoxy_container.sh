17b0420a38d612de28bcaee7b4642b8036df5a17
Roberto D'Auria <roberto@measurementlab.net>
2022-05-16 20:51:08 +0200

    Update go.15 -> go1.18 everywhere

--- deploy_epoxy_container.sh
+++ deploy_epoxy_container.sh
@@ -164,7 +164,7 @@ mkdir "${CERTDIR}"
 # Retry because docker fails to contact gcr.io sometimes.
 until docker run --rm --tty --volume /var/lib/toolbox:/tmp/go/bin \
   --env "GOPATH=/tmp/go" \
-  golang:1.15 /bin/bash -c \
+  golang:1.18 /bin/bash -c \
    "go get -u github.com/googlecloudplatform/gcsfuse &&
     apt-get update --quiet=2 &&
     apt-get install --yes fuse &&

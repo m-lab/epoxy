fc6d94ae3e6e01133c6e0ec53a995a3cdd00d468
Nathan Kinkade <kinkade@measurementlab.net>
2026-01-28 12:28:25 -0700

    Adds --yes flag to apt-install

--- deploy_epoxy_container.sh
+++ deploy_epoxy_container.sh
@@ -166,7 +166,7 @@ mkdir "${CERTDIR}"
 #
 # Add Docker's official GPG key:
 apt update
-apt install ca-certificates curl
+apt install --yes ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -179,7 +179,7 @@ Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF2
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install --yes docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Build the GCS fuse user space tools.
 # Retry because docker fails to contact gcr.io sometimes.

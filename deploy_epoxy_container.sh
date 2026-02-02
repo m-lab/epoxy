41296fb3caa605774e67dfbf25da15171dc1a5ec
Nathan Kinkade <kinkade@measurementlab.net>
2026-02-02 15:34:52 -0700

    Changes Docker install to install for Debian, not Ubuntu.
    I had mistakenly copied the install steps for Ubuntu, since that is the OS we
    generally use, but the ePoxy boot machine uses Debian, which is apparently the
    default image type when none is specified when creating the instance.
    
    Additionally, I failed to escape the version variable in the snippet, meaning
    that it was picking up the OS release of the Cloud Build environment, and _not_
    the OS release of the actual VM where ePoxy deploys.

--- deploy_epoxy_container.sh
+++ deploy_epoxy_container.sh
@@ -168,13 +168,13 @@ mkdir "${CERTDIR}"
 apt update
 apt install --yes ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 # Add the repository to Apt sources:
 tee /etc/apt/sources.list.d/docker.sources <<EOF2
 Types: deb
-URIs: https://download.docker.com/linux/ubuntu
-Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "\$VERSION_CODENAME}")
 Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF2
